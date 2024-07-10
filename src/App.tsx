cf13220bf3a28d0cdbf631a90eba7559375672f0
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2024-07-10 13:29:04 +0530

    removed unnecessary variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ContactUS from "./contactus";
-import HomePage from "./Homepage";
 
 function App() {
   return (
--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,9 +1,6 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import slide1 from "./assets/slide-01@2x.jpg";
-import slide2 from "./assets/slide-02@2x.jpg";
-import slide3 from "./assets/slide-03@2x.jpg";
-
 const HomePage = () => {
   return (
     <div className="w-screen h-screen">
